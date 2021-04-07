7c98924a2cf76d433faf3ef08523af9628d9c5d1
luisaLuna <luisacasaslu@gmail.com>
2021-04-07 13:55:12 -0500

    Second Practice

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const mainContainer = document.createElement('div');
 const h1 = document.createElement('h1');
 const h2 = document.createElement('h2');
-const button = document.createElement('button');
-const button2 = document.createElement('button');
+const btn1 = document.createElement('button');
+const btn2 = document.createElement('button');
+const image = document.createElement('img');
+        
 
-h1.innerHTML = 'This is an element created by JavaScript'
-h2.innerHTML = 'Click the below button to delete everything'
-button.innerHTML = 'Click me'
-button2.innerHTML = 'Reverse Everything'
+h1.innerHTML = 'Welcome! This text is coming from JavaScript'
+h2.innerHTML = 'Click the button below and you will see my favorite image'
+btn1.innerHTML = 'Click me'
+btn2.innerHTML = 'Delete image';
+
+image.src = 'https://images.unsplash.com/photo-1494913148647-353ae514b35e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80';
 
 document.body.append(mainContainer);
-mainContainer.append(h1, h2, button);
+mainContainer.append(h1, h2, btn1);
 
-button.addEventListener('click', () => {
-    h1.remove()
-    h2.remove()
-    button.remove()
 
-    mainContainer.append(button2);
+btn1.addEventListener('click', () => {
+    mainContainer.append(image, btn2);
+    btn1.remove()   
 })
 
-button2.addEventListener('click', () => {
-    button2.remove()
-    mainContainer.append(h1, h2, button);
+
+btn2.addEventListener('click', () => {
+    btn2.remove();
+    image.remove();
+    mainContainer.append(btn1);
 })
