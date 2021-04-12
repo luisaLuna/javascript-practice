6b2d61f8a224be98cf82b5c75ab28f864cedf04c
luisaLuna <luisacasaslu@gmail.com>
2021-04-12 09:43:19 -0500

    Date() practice

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 const mainContainer = document.createElement('div');
 const h1 = document.createElement('h1');
 const h2 = document.createElement('h2');
+const h2Time = document.createElement('h2');
+const date = document.createElement('p');
+const btn1 = document.createElement('button');
+const btn2 = document.createElement('button');
+let today = new Date();
         
 
 h1.innerHTML = 'Welcome! This text is coming from JavaScript'
-h2.innerHTML = 'Wait 3 seconds to see the background change color'
+h2.innerHTML = 'Click the button to see the current date and time'
+btn1.innerHTML = 'Click me'
+btn2.innerHTML = 'Delete'
+h2Time.innerHTML = 'The current date and time is:'
 
 document.body.append(mainContainer);
-mainContainer.append(h1, h2);
+mainContainer.append(h1, h2, btn1);
 
-let interval = setInterval(colorChange, 3000);
+btn1.addEventListener('click', () => {
+    h1.remove()
+    btn1.remove()
+    h2.remove()
+    date.innerHTML = `${today}`
+    mainContainer.append(h2Time, date, btn2);
+})
 
-function colorChange() {
-    let x = document.body;
-     x.style.backgroundColor = x.style.backgroundColor == "yellow" ? "#457b9d" : "yellow";
-}
+
+btn2.addEventListener('click', () => {
+    date.remove()
+    mainContainer.append(h1,h2,btn1);
+    btn2.remove()
+    h2Time.remove()
+})
