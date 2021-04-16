b1cf2b6321c16c7e98734eaee3733f436602eca2
luisaLuna <luisacasaslu@gmail.com>
2021-04-16 10:58:27 -0500

    For loop practice

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const mainContainer = document.createElement('div');
+const secondContainer = document.createElement('div');
+const buttonsContainer = document.createElement('div');
 const h1 = document.createElement('h1');
 const h2 = document.createElement('h2');
 const btn1 = document.createElement('button');
@@ -8,37 +10,43 @@ const p = document.createElement('p');
         
 
 h1.innerHTML = 'Welcome! This text is coming from JavaScript'
-h2.innerHTML = 'Click the button to change the page to dark mode'
-p.innerHTML = 'Click the button to change the page to light mode'
-btn1.innerHTML = 'Dark mode'
-btn2.innerHTML = 'Light mode'
+h2.innerHTML = 'Click the button to show 10 buttons!'
+p.innerHTML = 'Click the below button to delete the buttons'
+btn1.innerHTML = 'Click me'
+btn2.innerHTML = 'Click me'
 
 
 
-document.body.append(mainContainer);
+document.body.append(mainContainer, buttonsContainer, secondContainer);
 mainContainer.append(h1, h2, btn1);
 
 mainContainer.classList.add('mainContainer');
+secondContainer.classList.add('mainContainer');
+buttonsContainer.classList.add('buttonsContainer');
 btn1.classList.add('btn');
 btn2.classList.add('btn2');
 
 
-btn1.addEventListener('click', (e) => {
-    const html = document.querySelector('html');
-    html.classList.add('dark');
-    
-    mainContainer.append(p, btn2);
+btn1.addEventListener('click', () => {
+    for (let i = 0; i < 11; i++) {
+        let button = document.createElement('button');
+        button.innerText =`Button ${i+1}` ;
+        button.classList.add('buttons');
+        buttonsContainer.append(button);
+        secondContainer.append( p, btn2);
+    }
     btn1.remove()
     h2.remove()
+    h1.remove()
 })
 
-
 btn2.addEventListener('click', (e) => {
-    const html = document.querySelector('html');
-    html.classList.remove('dark');
-    
-    mainContainer.append(h2, btn1);
+    buttonsContainer.remove()
+    mainContainer.append(h1, h2, btn1);
     btn2.remove()
     p.remove()
 })
+
+
+
 
