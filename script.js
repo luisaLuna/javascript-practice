acc03d7a4ce44558a2be09eeea34b5c4f70af566
luisaLuna <luisacasaslu@gmail.com>
2021-04-23 11:46:27 -0500

    .addEventListener() Practice

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-const codes = document.querySelectorAll('.code');
+const container = document.querySelector('.container');
+const h1 = document.createElement('h1');
+const button = document.createElement('button');
+const buttonExtra = document.createElement('button');
+const image = document.createElement('img');
 
-codes[0].focus()
+h1.innerHTML = 'Hola! Esto es sólo una práctica';
+button.innerHTML = 'Click aquí';
+buttonExtra.innerHTML = 'Regresar';
+image.src = 'https://images.unsplash.com/photo-1585507252242-11fe632c26e8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80';
 
-codes.forEach((code, idx) => {
-    code.addEventListener('keydown', (e) => {
-        if (e.key >= 0 && e.key <= 9) {
-            codes[idx].value = ''
-            setTimeout(() => codes[idx + 1].focus(), 10);
-        } else if (e.key === 'Backspace') {
-             setTimeout(() => codes[idx - 1].focus(), 10);
-        }
-    })
+
+container.append(h1, button);
+
+button.addEventListener('click', () => {
+    container.append(image, buttonExtra);
+    button.remove()
+    h1.innerHTML = 'Esta mi imagen favorita:';
 })
 
-
-
-
+buttonExtra.addEventListener('click', () => {
+    container.append(button);
+    h1.innerHTML = 'Hola! Esto es sólo una práctica';
+    image.remove()
+    buttonExtra.remove()
+})
